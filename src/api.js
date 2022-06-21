27698d722662d6789ade06dc7df08caf2cb11f0b
qasimzia1 <qasimziak85@gmail.com>
2022-06-21 16:10:56 +0500

    popular game api

---
+++ src/api.js
@@ -0,0 +1,44 @@
+//base api 
+
+const base_url = "https://api.rawg.io/api/";
+
+//current month 
+const getcurrentmonth = ()=> {
+
+    const month = new Date().getMonth() + 1; 
+
+  if (month < 10 ) {
+    return `0${month}`;
+  } else {
+    return month;
+  }
+} ;
+
+//current day
+
+const getcurrentday = ()=> {
+
+    const day = new Date().getDay() + 19 ; 
+
+  if (day < 10 ) {
+    return `0${day}`;
+  } else {
+    return day;
+  }
+} ;
+
+//current day/month/year
+const currentyear = new Date().getFullYear();
+const currentmonth = getcurrentmonth();
+const currentday = getcurrentday(); 
+const currentdate = `${currentyear}-${currentmonth}-${currentday}`;
+const nextyear = `${currentyear + 1}-${currentmonth}-${currentday}`;
+const lastyear = `${currentyear - 1}-${currentmonth}-${currentday}`;
+
+
+//populargames 
+ 
+const populargames = `games?key=a75edc0889f6482a92aef3228cad5689&dates=${lastyear},${currentdate}&ordering=-rating&page_size=10`;
+
+ const populargamesurl= () => `${base_url}${populargames}`;
+ console.log(populargamesurl());
